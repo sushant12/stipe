3ea89f5a12a3b7eba310b7c14da61f4878447d93
sushant12 <sussyoung9@gmail.com>
2019-09-22 17:48:46 +0545

    install distillery

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Stipe.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:distillery, "~> 2.1", runtime: false},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
